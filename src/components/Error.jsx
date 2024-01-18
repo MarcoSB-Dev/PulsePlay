fac1efeff31a902f308c3fa09a0c4a1207d4046a
MarcoSB-Dev <marcobragado.cs@gmail.com>
2024-01-18 12:14:20 -0800

    Implement Music API fetching via rapidAPI.Implement Discover Page.

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Error = () => (
-  <div>Error</div>
+  <div className='w-full justify-center items-center'>
+
+    <h1 className='font-bold text-2xl text-white mt-2'>Error - Something Went Wrong..</h1>
+  </div>
 );
 
 export default Error;

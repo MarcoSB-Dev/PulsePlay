cf13d3d4f9568992f3c7161c91a03e1b746ec81f
MarcoSB-Dev <marcobragado.cs@gmail.com>
2024-01-19 19:15:42 -0800

    Implemented song details page with lyrics display and added endpoint query for specific song details

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,27 +1,28 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const options = {
     method: 'GET',
     headers: { 'X-RapidAPI-Key': '8fdae2cc9bmsh3a6c564f738898cp170e6fjsn645a101bfa39' }
 };
 
-    export const shazamCoreApi = createApi({
-        reducerPath: 'shazamCoreApi',
-        baseQuery: fetchBaseQuery({
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
 
-            //Shazam Core by WaveTech - FREE
-            // baseUrl:'https://shazam-core7.p.rapidapi.com',
+        //Shazam Core by WaveTech - FREE
+        // baseUrl:'https://shazam-core7.p.rapidapi.com',
 
-            //Shazam Core by Tipsters Co - $5/mo
-            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-            prepareHeaders: (headers) => {
-                headers.set('X-RapidAPI-Key','8fdae2cc9bmsh3a6c564f738898cp170e6fjsn645a101bfa39')
-                return headers;
-            },
-        }),
-        endpoints: (builder)=> ({
-            getTopCharts: builder.query({ query: () => '/charts/world'}),
-        }),
-    });
+        //Shazam Core by Tipsters Co - $5/mo
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', '8fdae2cc9bmsh3a6c564f738898cp170e6fjsn645a101bfa39')
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+        getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` })
+    }),
+});
 
-    export const {useGetTopChartsQuery} = shazamCoreApi;
+export const { useGetTopChartsQuery, useGetSongDetailsQuery } = shazamCoreApi;

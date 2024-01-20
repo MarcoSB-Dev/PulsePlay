8b0f9b1ec870c57d7322ecec7d2ec3fde5f5ee03
MarcoSB-Dev <marcobragado.cs@gmail.com>
2024-01-20 12:30:55 -0800

    Implemented search bar component + funcitonality

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,38 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searrchItem, setSearchItem] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${searrchItem}`);
+  }
+
+  return (
+    <form
+      autoComplete="off"
+      className="p-2 text-gray-500 focus-within:text-gray-400"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="search-field" className="sr-only"> Search </label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch className="w-5 h-10 ml-3" />
+        <input name="search-field"
+          autoComplete="off"
+          id="search-field"
+          placeholder="Search"
+          type="search"
+          value={searrchItem}
+          onChange={(e) => setSearchItem(e.target.value)}
+          className="w-full h-10 ml-3 p-4 bg-transparent focus:outline-none text-white text-lg"
+        />
+      </div>
+    </form>
+  )
+
+};
 
 export default Searchbar;
